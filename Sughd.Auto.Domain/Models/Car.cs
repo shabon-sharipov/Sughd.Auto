dc39b2f7f3c0d1689d942505d94b383b889dfd77
Shabon Sharipov <shabon@snoonu.com>
2024-02-28 09:25:32 +0500

    Created Car, Color, Marka, and CarBody models

---
+++ Sughd.Auto.Domain/Models/Car.cs
@@ -0,0 +1,41 @@
+﻿using Sughd.Auto.Domain.Abstract;
+using Sughd.Auto.Domain.Enum;
+
+namespace Sughd.Auto.Domain.Models;
+
+public class Car : EntityBase
+{
+    public string Name { get; set; }
+
+    public string VINCode { get; set; }
+
+    public string DateOfPablisher { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string EngineCapacity { get; set; }
+
+    public decimal Mileage { get; set; }
+
+    public double IsRastamogeno { get; set; }
+
+    public List<string> Images { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public FuelType FuelType { get; set; }
+
+    public Transmission Transmission { get; set; }
+
+    public CarBody CarBody { get; set; }
+    public ulong CarBodyId { get; set; }
+
+    public Customer Customer { get; set; }
+    public ulong CustomerId { get; set; }
+
+    public Marka Marka { get; set; }
+    public ulong MarkaId { get; set; }
+
+    public Model Model { get; set; }
+    public ulong ModelId { get; set; }
+}
---
+++ Sughd.Auto.Domain/Models/Marka.cs
@@ -0,0 +1,15 @@
+﻿using Sughd.Auto.Domain.Abstract;
+
+namespace Sughd.Auto.Domain.Models;
+
+public class Marka : EntityBase
+{
+    public string Name { get; set; }
+
+    public List<Model> Model { get; set; }
+}
+
+public class Model : EntityBase
+{
+    public string Name { get; set; }
+}
